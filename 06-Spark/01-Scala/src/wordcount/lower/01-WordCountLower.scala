3458691ad78b7ec4f784926739cdbbb57df90be0
terodea <terodeakshay@gmail.com>
2021-02-10 02:08:37 +0530

    Spark: Lower, Optimized, Function Chaining

---
+++ 06-Spark/01-Scala/src/wordcount/lower/01-WordCountLower.scala
@@ -0,0 +1,19 @@
+package wordcount.lower
+
+import org.apache.spark.SparkContext
+import org.apache.log4j.Level
+import org.apache.log4j.Logger
+
+object WordCountLower extends App {
+  /*
+   * A word count program that converts word into lower case.
+   * Function Chaining also implemented.
+   * 
+   * */
+  Logger.getLogger("org").setLevel(Level.ERROR)
+  val sc = new SparkContext("local[*]", "wordcountLower")
+  sc.textFile("").
+  flatMap(_.split(" ")).
+  map(_.toLowerCase()).
+  map((_, 1)).reduceByKey(_+_).collect.foreach(println)
+}
