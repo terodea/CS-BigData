e1e8d255aedb74aa38c7a2efb9d162ee65692c52
terodea <terodeakshay@gmail.com>
2022-02-22 01:25:52 +0530

    additional helpers

--- 06-Spark/02-Python/helpers.py
+++ 06-Spark/02-Python/helpers.py
@@ -106,3 +106,10 @@ def _group(data, by, n_partitions=None):
     else:
         return rdd.groupBy(lambda x: x[by])
 
+def column_to_list(df, col_name):
+    return [x[col_name] for x in df.select(col_name).collect()]
+
+
+def two_columns_to_dictionary(df, key_col_name, value_col_name):
+    k, v = key_col_name, value_col_name
+    return {x[k]: x[v] for x in df.select(k, v).collect()}
