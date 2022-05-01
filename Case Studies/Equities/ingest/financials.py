a8672377caddb4a83e42df37b68838488e0f8c11
terodea <terodeakshay@gmail.com>
2022-05-02 00:00:32 +0530

    Equties ds added

--- Case Studies/Equities/ingest/financials.py
+++ Case Studies/Equities/ingest/financials.py
@@ -34,29 +34,37 @@ class IngestFinancialData:
         self, stock_name: AnyStr="", target_table: AnyStr="profit-loss", table_clss:AnyStr="data-table responsive-text-nowrap"
     ):
         try:
-            
+            self.read_source()
             session = self.session or _requests
-            stock_name = stock_name.split('.NS')[0] if ".NS" in stock_name else stock_name
+            stock_names = self.stock_names if self.stock_names else (stock_name.split('.NS')[0] if ".NS" in stock_name else stock_name)
 
-            crawled_data = session.get(f"https://www.screener.in/company/{stock_name}/")
-            stock_soup = BeautifulSoup(crawled_data.text, "html.parser")
-            pl_table = stock_soup.find("section", {"id":target_table}).find("table", {"class": table_clss})
-            df = pd.DataFrame(columns = [i.text for i in pl_table.find_all("th")])
+            for stock_name in stock_names:
+                crawled_data = session.get(f"https://www.screener.in/company/{stock_name}/")
+                stock_soup = BeautifulSoup(crawled_data.text, "html.parser")
+                pl_table = stock_soup.find("section", {"id":target_table}).find("table", {"class": table_clss})
+                df = pd.DataFrame(columns = [i.text for i in pl_table.find_all("th")])
 
-            for j in pl_table.find_all("tr")[1:]:
-                row_val = []
-                for i in j.find_all("td"):
-                    _val = i.text
-                    if self.is_digit(_val) or self.is_comma_numeric(_val) or self.is_percent(_val):
-                        row_val.append(_val)
-                    else:
-                        row_val.append(" ".join(re.findall("[a-zA-Z0-9]+", _val)))
-                df.loc[len(df)] = row_val
-            
-            return df.to_dict(orient="records")
+                for j in pl_table.find_all("tr")[1:]:
+                    row_val = []
+                    for i in j.find_all("td"):
+                        _val = i.text
+                        if self.is_digit(_val) or self.is_comma_numeric(_val) or self.is_percent(_val):
+                            row_val.append(_val)
+                        else:
+                            row_val.append(" ".join(re.findall("[a-zA-Z0-9]+", _val)))
+                    df.loc[len(df)] = row_val
+                
+                return df.to_dict(orient="records")
         except Exception as err:
             self.logger.error(err, exc_info=True)
 
+    def read_source(self, target_col:AnyStr=""):
+        try:
+            self.df = pd.read_csv("Case Studies/Equities/ingest/data/bse-500-index-01-May-2022--0054.xls")
+            self.stock_names =  [f"{i}.NS" for i in self.df["Nse code"].unqiue().tolist()]
+        except Exception as err:
+            self.logger.error(msg=err, exc_info=True)
+            raise err
 
 if __name__ == "__main__":
     obj = IngestFinancialData()
